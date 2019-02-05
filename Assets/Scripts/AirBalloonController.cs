6636ac552e0232e082c746a52b2aea6370744074
Mary Gibeau <marygold1011@gmail.com>
2019-02-05 11:04:37 -0500

    Parchment collision detection

---
+++ Assets/Scripts/AirBalloonController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AirBalloonController : MonoBehaviour
+{
+   
+   public Rigidbody2D rb;
+   public Text paperText;
+   public float jumpHeight;
+   private int paperCounter;
+
+    private void Start() {
+        paperCounter = 0;
+        // paperText = GetComponent<Text>();
+    }
+
+    void FixedUpdate() {
+        if (Input.GetKeyDown(KeyCode.Space)) { 
+            rb.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            rb.AddForce(new Vector2(-2, 0), ForceMode2D.Impulse);
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            rb.AddForce(new Vector2(2, 0), ForceMode2D.Impulse);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        Debug.Log("collision!");
+        if (col.gameObject.tag == "paper") {
+            paperCounter++;
+            paperText.text = "Papers Collected: " + paperCounter;
+            Destroy(col.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PaperSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaperSpawner : MonoBehaviour
+{
+    public GameObject paperPrefab;
+    public float spawnTime = 2f;
+    public int paperSpeed;
+    private Vector2 spawnPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+        paperSpeed = -100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Spawn () {
+        spawnPosition.x = 250f;
+        spawnPosition.y = Random.Range(-200, 200);
+        var spawnedPaper = Instantiate(paperPrefab, spawnPosition, Quaternion.Euler(0, 0, 0));
+        spawnedPaper.GetComponent<Rigidbody2D>().velocity = paperSpeed * transform.localScale.x * spawnedPaper.transform.right;
+    }
+}
