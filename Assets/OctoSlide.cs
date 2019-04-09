8ce197d24cf751da90116506f1ad035a32d84449
grahamymac <grahamymac@yahoo.com>
2019-04-09 11:58:10 -0400

    added octo animation

---
+++ Assets/OctoSlide.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OctoSlide : MonoBehaviour
+{
+    Animator anm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anm = GetComponent<Animator> ();
+        anm.Play ("SquidAnimation");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
