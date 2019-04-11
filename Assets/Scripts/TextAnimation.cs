971e247918b9799dee7024273189beb759a2dbb8
grahamymac <grahamymac@yahoo.com>
2019-04-11 11:36:21 -0400

    text animations

---
+++ Assets/Scripts/TextAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextSlide : MonoBehaviour
+{
+    Animator anm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anm = GetComponent<Animator> ();
+        anm.Play ("TextSlide");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
